a859c5e760e860bddb4401809948fd5a4f96f75f
Patrick Baselier <patrick.baselier@gmail.com>
2019-03-31 19:29:29 +0200

    Replace model w/ fetch

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,20 +1,40 @@
 import Controller from "@ember/controller";
+import fetch from "fetch";
 
 export default Controller.extend({
   tokenRequested: false,
   actions: {
     requestToken() {
-      let session = this.store.createRecord("session", this.model);
-
-      session
-        .save()
-        .then(() => {
+      this._postData()
+        .then(res => {
           this.set("tokenRequested", true);
         })
         .catch(e => {
-          this.set("errors", session.get("errors"));
-          session.deleteRecord();
+          const errs = e.errors.map(err => err.detail);
+          this.set("errors", errs);
+          console.error(errs);
         });
     }
+  },
+  _postData() {
+    const url = "http://localhost:3000/sessions";
+    const data = { email: this.model.email };
+
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then(res => {
+      let json = res.json();
+      if (res.ok) {
+        return json;
+      } else {
+        return json.then(err => {
+          throw err;
+        });
+      }
+    });
   }
 });
